962490f046191ce9497b177d71f241249079070b
faduma abdi <faduma_abdi@hotmail.co.uk>
2023-08-23 17:36:25 +0100

    thought routes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,33 @@
+const router = require('express').Router();
+const {
+  getAllThought,
+  getThoughtById,
+  createThought,
+  updateThought,
+  deleteThought,
+  createReaction,
+  deleteReaction
+} = require('../../controllers/thoughtController.js');
+
+// /api/thoughts GET/POST all
+router.route('/').get(getAllThought).post(createThought);
+
+// /api/courses/:thoughtId GET/PUT/DELETE single
+router
+  .route('/:id')
+  .get(getThoughtById)
+  .put(updateThought)
+  .delete(deleteThought);
+
+
+// posts
+router
+  .route('/:thoughtId/reactions')
+  .post(createReaction);
+
+router.route('/:thoughtId/reactions/:reactionId')
+.delete(deleteReaction);
+
+
+
+module.exports = router;
