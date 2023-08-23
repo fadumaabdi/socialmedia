87d6e110e5feddcd95e342956a1cb2cd87a73f06
faduma abdi <faduma_abdi@hotmail.co.uk>
2023-08-23 17:45:30 +0100

    import api routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
+//import all API routes from api/index.js
 const apiRoutes = require('./api');
-
+//add api prefix
 router.use('/api', apiRoutes);
 
 router.use((req, res) => res.send('Wrong route!'));
