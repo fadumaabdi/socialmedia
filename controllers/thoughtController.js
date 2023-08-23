56c423a4abc3864e861274b4daabe7856cfbf6e8
faduma abdi <faduma_abdi@hotmail.co.uk>
2023-08-23 18:03:47 +0100

    update thought

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -50,20 +50,20 @@ module.exports = {
       res.status(500).json(err);
     }
   },
-  // Update a course
-  async updateCourse(req, res) {
+  // Update a thought
+  async updateThought(req, res) {
     try {
-      const course = await Course.findOneAndUpdate(
-        { _id: req.params.courseId },
+      const thought = await Thought.findOneAndUpdate(
+        { _id: req.params.thoughtId },
         { $set: req.body },
         { runValidators: true, new: true }
       );
 
-      if (!course) {
-        return res.status(404).json({ message: 'No course with this id!' });
+      if (!thought) {
+        return res.status(404).json({ message: 'No thought with this id!' });
       }
 
-      res.json(course);
+      res.json(thought);
     } catch (err) {
       res.status(500).json(err);
     }
