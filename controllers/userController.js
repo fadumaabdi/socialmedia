8de7c9ebae93557223b1e61dc167aae19f53b993
faduma abdi <faduma_abdi@hotmail.co.uk>
2023-08-24 20:26:06 +0100

    mongoose

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,5 @@
 // ObjectId() method for converting studentId string into an ObjectId for querying database
-// const { ObjectId } = require('mongoose').Types;
+const { ObjectId } = require('mongoose').Types;
 const { User, Thought } = require('../models');
 
 
